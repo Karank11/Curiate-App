db0d120c9e39c563b9c8ce48e266c0cba589c92e
karankarhale <karanp.karhale@gmail.com>
2025-08-20 08:54:05 +0530

    create bottombar and appbar

--- app/src/main/java/com/example/curiate/MainActivity.kt
+++ app/src/main/java/com/example/curiate/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.curiate
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.Menu
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        setupSearchBar(menu)
+        return true
+    }
+
+    private fun setupSearchBar(menu: Menu?) {
+        val searchItem = menu?.findItem(R.id.action_search)
+        val searchView = searchItem?.actionView as SearchView
+        val pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 280f, resources.displayMetrics).toInt()
+        searchView.maxWidth = pixels
+        val searchEditText = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+        searchEditText.background = ContextCompat.getDrawable(this, R.drawable.search_bar_bg)
+        searchEditText.setTextColor(Color.BLACK)
+        searchEditText.setHintTextColor(Color.GRAY)
     }
 }
