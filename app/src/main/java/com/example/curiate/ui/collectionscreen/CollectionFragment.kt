69a43db16be4810e388ba711bedb2890d40f9814
karankarhale <karanp.karhale@gmail.com>
2025-08-20 14:57:28 +0530

    enable viewBinding and dataBinding

--- app/src/main/java/com/example/curiate/ui/collectionscreen/CollectionFragment.kt
+++ app/src/main/java/com/example/curiate/ui/collectionscreen/CollectionFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.curiate.R
+import com.example.curiate.databinding.FragmentCollectionBinding
 
 class CollectionFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_collection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding: FragmentCollectionBinding = DataBindingUtil.setContentView(requireActivity(), R.layout.fragment_collection)
     }
 }
