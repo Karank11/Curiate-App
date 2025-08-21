f9eb1b79dcdd7a8999d29172c73147b956729d67
karankarhale <karanp.karhale@gmail.com>
2025-08-21 20:14:03 +0530

    create preview card

--- app/src/main/java/com/example/curiate/ShareReceiverActivity.kt
+++ app/src/main/java/com/example/curiate/ShareReceiverActivity.kt
@@ -1,5 +1,6 @@
 package com.example.curiate
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.curiate.ui.SaveContentFragment
@@ -7,7 +8,16 @@ import com.example.curiate.ui.SaveContentFragment
 class ShareReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val saveContentFragment = SaveContentFragment.newInstance("url")
-        saveContentFragment.show(supportFragmentManager, SaveContentFragment.TAG)
+        if (intent?.action == Intent.ACTION_SEND && intent.type == "text/plain") {
+            val args = Bundle()
+            intent.getStringExtra(Intent.EXTRA_TEXT)?.let {text ->
+                args.putString(Intent.EXTRA_TEXT, text)
+            }
+            intent.getStringExtra(Intent.EXTRA_SUBJECT)?.let {subject ->
+                args.putString(Intent.EXTRA_SUBJECT, subject)
+            }
+            val saveContentFragment = SaveContentFragment.newInstance(args)
+            saveContentFragment.show(supportFragmentManager, SaveContentFragment.TAG)
+        }
     }
 }
--- app/src/main/java/com/example/curiate/ui/SaveContentFragment.kt
+++ app/src/main/java/com/example/curiate/ui/SaveContentFragment.kt
@@ -1,43 +1,126 @@
 package com.example.curiate.ui
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.Group
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import com.example.curiate.R
+import com.example.curiate.domain.models.LinkPreviewData
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
 
 class SaveContentFragment : BottomSheetDialogFragment() {
+    companion object {
+        const val TAG = "SaveContentFragment"
+        fun newInstance(args: Bundle): SaveContentFragment {
+            val fragment = SaveContentFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+    private lateinit var contentImageView: ImageView
+    private lateinit var contentTitleView: TextView
+    private lateinit var contentUrlView: TextView
+    private lateinit var contentGroup: Group
+    private lateinit var loadingGroup: Group
+    private lateinit var closeButton: ImageButton
+    private lateinit var saveButton: Button
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_save_content, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        contentImageView = view.findViewById(R.id.content_image)
+        contentTitleView = view.findViewById(R.id.content_title)
+        contentUrlView = view.findViewById(R.id.content_url)
+        contentGroup = view.findViewById(R.id.content_group)
+        loadingGroup = view.findViewById(R.id.loading_group)
+        closeButton = view.findViewById(R.id.close_button)
+        saveButton = view.findViewById(R.id.save_button)
 
-        val closeButton: ImageButton = view.findViewById(R.id.close_button)
+        saveButton.isEnabled = false
+        loadingGroup.visibility = View.VISIBLE
+        contentGroup.visibility = View.GONE
+        val args = arguments ?: return
+        val text = args.getString(Intent.EXTRA_TEXT)
+        var initialTitle = args.getString(Intent.EXTRA_SUBJECT)
+        val sharedUrl = findUrlInText(text ?: "")
+        if (sharedUrl == null) {
+            dismiss()
+            return
+        }
+        if (initialTitle.isNullOrEmpty()) {
+            initialTitle = text?.substringBefore(sharedUrl)?.trim()
+        }
+        lifecycleScope.launch {
+            val previewData = fetchLinkPreview(sharedUrl)
+            contentTitleView.text = previewData.title.ifEmpty { initialTitle ?: "" }
+            contentUrlView.text = previewData.contentUrl
+
+            Glide.with(this@SaveContentFragment)
+                .load(previewData.imageUrl)
+                .placeholder(R.drawable.ic_placeholder_image)
+                .into(contentImageView)
+
+            saveButton.isEnabled = true
+            contentGroup.visibility = View.VISIBLE
+            loadingGroup.visibility = View.GONE
+        }
+
+        saveButton.setOnClickListener {
+            // Handle save button click
+            dismiss()
+        }
 
         closeButton.setOnClickListener {
             dismiss()
         }
+    }
 
+    /**
+     * Finds the first URL in a given block of text.
+     * @param text The text to search within.
+     * @return The first found URL as a String, or null if no URL is found.
+     */
+    private fun findUrlInText(text: String): String? {
+        val matcher = Patterns.WEB_URL.matcher(text)
+        return if (matcher.find()) {
+            matcher.group()
+        } else {
+            null
+        }
+    }
+
+    private suspend fun fetchLinkPreview(sharedUrl: String): LinkPreviewData {
+        return withContext(Dispatchers.IO) {
+            val doc = Jsoup.connect(sharedUrl).get()
+            val title = doc.select("meta[property=og:title]").attr("content").ifEmpty {
+                doc.title()
+            } ?: ""
+            val imageUrl = doc.select("meta[property=og:image]").attr("content") ?: ""
+            val contentUrl = doc.select("meta[property=og:url]").attr("content") ?: ""
+            LinkPreviewData(imageUrl, title, contentUrl)
+        }
     }
 
     override fun onDismiss(dialog: DialogInterface) {
         super.onDismiss(dialog)
         activity?.finish()
-    }
-
-    companion object {
-        const val TAG = "SaveContentFragment"
-        fun newInstance(url: String): SaveContentFragment {
-            val args = Bundle()
-            args.putString("url", url)
-            val fragment = SaveContentFragment()
-            fragment.arguments = args
-            return fragment
-        }
     }
 }
