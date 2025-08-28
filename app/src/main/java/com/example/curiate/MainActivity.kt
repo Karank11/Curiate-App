c983f59302d5bd0d945a1827e6dbe7c7815c0910
karankarhale <55053323+Karank11@users.noreply.github.com>
2025-08-28 20:56:54 +0530

    fix edge-to-edge

--- app/src/main/java/com/example/curiate/MainActivity.kt
+++ app/src/main/java/com/example/curiate/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         setupBottomBar()
 
         enableEdgeToEdge()
-        setContentView(R.layout.activity_random)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
